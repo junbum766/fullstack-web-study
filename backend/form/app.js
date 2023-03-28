d42c3111c25c7adf11abbaac29ed867dbc6e0dc6
hong <hong@macui-MacBookPro.local>
2023-03-28 19:51:56 +0900

    new

--- backend/form/app.js
+++ backend/form/app.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const app = express();
-const PORT = 1234;
+const PORT = 2000;
 
 app.set("view engine", "ejs");
 
 app.use("/views", express.static(__dirname + "/views"));
 
+app.use(express.urlencoded({ extended: true })); // post 요청으로 들어오는 모든형삭의 데아터를 parsing
+app.use(express.json()); // json 형태로 데이터를 주고 받음
+
 app.get("/", (req, res) => {
-  res.render("index");
+  const myTitle = "폼 실습을 해보자!";
+  res.render("index", { title: myTitle, name: "hong" });
 });
 
 app.listen(PORT, () => {
   console.log(PORT + " port가 연결되었습니다.");
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.get("/getForm", function (req, res) { // 웹에 아이디 비번이 표시됨..
+  console.log(req.query);
+  res.send("get 요청 성공!");
+});
+
+app.post("/postForm", function (req, res) { // 웹에 아이디 비번을 가려줌
+  console.log(req.body);
+//   console.log(req)
+  res.send("post 요청 성공!");
+});

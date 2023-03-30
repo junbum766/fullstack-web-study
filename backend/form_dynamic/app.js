9c31675b693873ace1641116152e98acb2b4d66f
hong <hong@macui-MacBookPro.local>
2023-03-30 19:25:49 +0900

    new

--- backend/form_dynamic/app.js
+++ backend/form_dynamic/app.js
@@ -26,6 +26,18 @@ app.post("/ajax", (req, res) => {
   res.send(req.body);
 });
 
+// 2-1. axios get
+app.get("/axios", (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+// 2-2. axios post
+app.post("/axios", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 app.listen(PORT, () => {
   console.log(PORT + "port 가 연결되었습니다.");
   console.log('접속 : http://localhost:'+PORT);

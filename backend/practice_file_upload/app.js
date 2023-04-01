47f2a3c5362e4f852153bb3f5b1cf3b119fc2c53
hong <hong@macui-MacBookPro.local>
2023-04-01 11:37:40 +0900

    230401 new

---
+++ backend/practice_file_upload/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const multer = require("multer");
+const path = require("path");
+
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, req.body.id + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/result", uploadDetail.single("userfile"), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+  res.render("result", { file: req.file, body: req.body });
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});

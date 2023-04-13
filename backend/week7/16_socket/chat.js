953ab9f527949210d09cb5d66cf06d9817264767
junbum766 <junbum766@gmail.com>
2023-04-13 22:31:54 +0900

    230413 new

--- backend/week7/16_socket/chat.js
+++ backend/week7/16_socket/chat.js
@@ -13,6 +13,17 @@ app.get("/", function (req, res) {
   res.render("chat");
 });
 
+// 닉네임 저장할 객체
+// : 닉네임이 겹치지 않도록 객체를 사용함
+// : {socket_id : nickname, ...}
+const nickObj = {};
+
+// 닉네임 리스트 객체 업데이트(유저가 들어가거나 나왔을 때)
+function updateNicList() {
+  io.emit("updateNicks", nickObj);
+  // 서버에 접속한 클라이언트들에게 nickObj에 변경이 일어났음을 알리는 이벤트
+}
+
 io.on("connection", (socket) => {
   // 'connection' 이벤트
   // - 클라이언트가 접속했을 때 발생하는 이벤트
@@ -21,28 +32,68 @@ io.on("connection", (socket) => {
   console.log("⭕ Server Socket Connected >>> ", socket.id);
 
   // [실습1]
-  socket.on("hello", (data) => {
-    console.log(`${data.who}: ${data.msg}`);
-    // server -> client
-    socket.emit("hellokr", { who: "server", msg: "안녕!" });
-  });
+  // socket.on("hello", (data) => {
+  //   console.log(`${data.who}: ${data.msg}`);
+  //   // server -> client
+  //   socket.emit("hellokr", { who: "server", msg: "안녕!" });
+  // });
 
-  socket.on("study", (data) => {
-    console.log(`${data.who}: ${data.msg}`);
-    // server -> client
-    socket.emit("studykr", { who: "server", msg: "공부해!" });
-  });
+  // socket.on("study", (data) => {
+  //   console.log(`${data.who}: ${data.msg}`);
+  //   // server -> client
+  //   socket.emit("studykr", { who: "server", msg: "공부해!" });
+  // });
 
-  socket.on("bye", (data) => {
-    console.log(`${data.who}: ${data.msg}`);
-    // server -> client
-    socket.emit("byekr", { who: "server", msg: "빠이!" });
-  });
+  // socket.on("bye", (data) => {
+  //   console.log(`${data.who}: ${data.msg}`);
+  //   // server -> client
+  //   socket.emit("byekr", { who: "server", msg: "빠이!" });
+  // });
 
-  // [실습2]
+  // [실습2, 4]
   socket.on("chat", (data) => {
-    console.log("서버 >>> ", `${data.who}: ${data.msg}`);
-    socket.emit("chat", { who: "hong", msg: "hi" });
+    const sendData = {
+      nick: data.myNick,
+      msg: data.input,
+      to: data.to,
+      obj: nickObj,
+    };
+    console.log("check....", data);
+    if (data.to === "all") {
+      console.log("all >>>", data.to, "에게 <<<");
+      io.emit("ioChat", sendData);
+    } else {
+      console.log("direct >>>", data.to, "에게 <<<");
+      io.to(data.to).emit("ioChat", sendData);
+      socket.emit("ioChat", sendData); // io.to 는 목적지에만 가기 때문에, 나 자신의 채팅 창에도 반영하기 위해서 socket.emit을 추가한다.
+    }
+  });
+
+  // [실습3] 채팅창 입장 안내
+  // io.emit("notice", `${socket.id.slice(0, 5)}님이 입장하셨습니다.`);
+
+  // [실습 3-2] 닉네임 셋팅
+  socket.on("setNick", (nickname) => {
+    if (Object.values(nickObj).indexOf(nickname) > -1) {
+      // id 중복
+      console.log(nickObj);
+      socket.emit("error", "이미 존재하는 아이디 입니다. 다시 입력해 주세요!");
+    } else {
+      nickObj[socket.id] = nickname;
+      io.emit("notice", `${nickname}님이 입장하셨습니다.`); // 입장 알림
+      socket.emit("entrySuccess", nickname); // 입장 성공시 이벤트
+      console.log(nickObj);
+      updateNicList(); // nickObj 업데이트 알림
+    }
+  });
+
+  // [실습 3-3] 퇴장 : emit하는 코드는 없지만 창을 닫는 이벤트를 받음
+  socket.on("disconnect", () => {
+    console.log("❌ Server Socket Disconneted >> ", socket.id);
+    io.emit("notice", `${nickObj[socket.id]}님이 퇴장하셨습니다.`);
+    delete nickObj[socket.id];
+    updateNicList(); // nickObj 업데이트 알림
+    console.log(nickObj);
   });
 });
 
--- backend/week7/16_socket/static/index.js
+++ backend/week7/16_socket/static/index.js
@@ -1,46 +1,50 @@
 // socket 사용을 위한 객체 생성
 let socket = io.connect();
 
+// 닉네임 셋팅
+let myNick;
+
 socket.on("connect", () => {
   console.log("⭕ Client Socket Connected >> ", socket.id);
 });
+// [실습1]
+// function sayHello() {
+//   // client -> server 정보 보내기
+//   // socket.emit(event, data): 데이터 전송
+//   // => event 라는 이름으로 data 전송
+//   socket.emit("hello", { who: "client", msg: "hello" });
 
-function sayHello() {
-  // client -> server 정보 보내기
-  // socket.emit(event, data): 데이터 전송
-  // => event 라는 이름으로 data 전송
-  socket.emit("hello", { who: "client", msg: "hello" });
+//   // socket.on(event, callback): 데이터 받음
+//   // event에 대해서 정보를 받아 callback 함수 실행
+//   socket.on("hellokr", (data) => {
+//     console.log(`${data.who}: ${data.msg}`);
+//     document.querySelector(
+//       "#form-server"
+//     ).textContent = `${data.who}: ${data.msg}`;
+//   });
+// }
 
-  // socket.on(event, callback): 데이터 받음
-  // event에 대해서 정보를 받아 callback 함수 실행
-  socket.on("hellokr", (data) => {
-    console.log(`${data.who}: ${data.msg}`);
-    document.querySelector(
-      "#form-server"
-    ).textContent = `${data.who}: ${data.msg}`;
-  });
-}
+// function sayStudy() {
+//   socket.emit("study", { who: "client", msg: "study" });
+//   socket.on("studykr", (data) => {
+//     console.log(`${data.who}: ${data.msg}`);
+//     document.querySelector(
+//       "#form-server"
+//     ).textContent = `${data.who}: ${data.msg}`;
+//   });
+// }
 
-function sayStudy() {
-  socket.emit("study", { who: "client", msg: "study" });
-  socket.on("studykr", (data) => {
-    console.log(`${data.who}: ${data.msg}`);
-    document.querySelector(
-      "#form-server"
-    ).textContent = `${data.who}: ${data.msg}`;
-  });
-}
+// function sayBye() {
+//   socket.emit("bye", { who: "client", msg: "bye" });
+//   socket.on("byekr", (data) => {
+//     console.log(`${data.who}: ${data.msg}`);
+//     document.querySelector(
+//       "#form-server"
+//     ).textContent = `${data.who}: ${data.msg}`;
+//   });
+// }
 
-function sayBye() {
-  socket.emit("bye", { who: "client", msg: "bye" });
-  socket.on("byekr", (data) => {
-    console.log(`${data.who}: ${data.msg}`);
-    document.querySelector(
-      "#form-server"
-    ).textContent = `${data.who}: ${data.msg}`;
-  });
-}
-
+// [실습2, 4]
 function enterkey() {
   if (window.event.keyCode == 13) {
     chatSubmit();
@@ -48,22 +52,83 @@ function enterkey() {
 }
 
 function chatSubmit() {
-  console.log("제출");
-  socket.emit("chat", { who: "hong", msg: "hi" });
-  socket.on("chat", (data) => {
-    console.log("클라이언트 >>> ", `${data.who}: ${data.msg}`);
-    const input = document.querySelector("#chat-input").value;
-    trimedInput = input.trim();
-    if (trimedInput !== "") {
-      const chatBox = `<div class='chatR'>${input}</div>`;
+  const input = document.querySelector("#chat-input").value;
+  const to = document.querySelector("#nick-list").value;
+  trimedInput = input.trim();
+  if (trimedInput !== "") {
+    socket.emit("chat", { input: input, myNick: myNick, to: to });
+    document.querySelector("#chat-input").value = "";
+  }
+}
 
-      document
-        .querySelector(".chat-box")
-        .insertAdjacentHTML("beforeend", chatBox);
-      document.querySelector("#chat-input").value = "";
-    }
-  });
+socket.on("ioChat", (data) => {
+  // 상대방 채팅창에 반영
+  if (myNick != data.nick && data.to == "all") {
+    const chatBox = `<div class='chatL'>${data.nick}: ${data.msg}</div>`;
+    document
+      .querySelector(".chat-box")
+      .insertAdjacentHTML("beforeend", chatBox);
+  } else if (myNick != data.nick && data.to != "all") {
+    const chatBox = `<div class='chatL-dir'>(direct from ${data.nick}) ${data.msg}</div>`;
+    document
+      .querySelector(".chat-box")
+      .insertAdjacentHTML("beforeend", chatBox);
+  }
+  // 내 채팅창에 반영
+  if (myNick == data.nick && data.to == "all") {
+    const chatBox = `<div class='chatR'>${data.msg} :나</div>`;
+    document
+      .querySelector(".chat-box")
+      .insertAdjacentHTML("beforeend", chatBox);
+  } else if (myNick == data.nick && data.to != "all") {
+    const chatBox = `<div class='chatR-dir'>${data.msg} :나(direct to ${
+      data.obj[data.to]
+    })</div>`;
+    document
+      .querySelector(".chat-box")
+      .insertAdjacentHTML("beforeend", chatBox);
+  }
 
   document.querySelector(".chat-box").scrollTop =
     document.querySelector(".chat-box").scrollHeight; // 스크롤 자동 다운
+});
+
+// [실습3] 채팅창 입장 안내
+socket.on("notice", (msg) => {
+  document
+    .querySelector(".chat-box")
+    .insertAdjacentHTML("beforeend", `<div class='notice'>${msg}</div>`);
+});
+
+// [실습 3-2] 닉네임 셋팅
+function entry() {
+  socket.emit("setNick", document.querySelector("#nickname").value);
 }
+
+socket.on("entrySuccess", (nickname) => {
+  // 닉네임 설정
+  myNick = nickname;
+
+  // 채팅 박스 나타나게
+  document.querySelector(".container").classList.remove("d-none");
+
+  // 닉네임 인풋, 버튼 비활성화
+  document.querySelector("#nickname").disabled = true;
+  document.querySelector("#nickname-btn").disabled = true;
+});
+
+socket.on("error", (msg) => {
+  alert(msg);
+  document.querySelector("#nickname").value = "";
+});
+
+// 유저 목록 업데이트
+socket.on("updateNicks", (obj) => {
+  let options = '<option value="all">전체</option>';
+  // select#nick-list 요소 옵션 추가
+  for (let key in obj) {
+    options += `<option value="${key}">${obj[key]}</option>`;
+  }
+  // select 요소에 options 덮어쓰기
+  document.querySelector("#nick-list").innerHTML = options;
+});

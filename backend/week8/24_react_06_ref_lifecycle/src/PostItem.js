d3f4dd6bd324666c4182aae1a17355d2caaf9a31
junbum766 <junbum766@gmail.com>
2023-04-24 16:56:18 +0900

    230424 new

--- backend/week8/24_react_06_ref_lifecycle/src/PostItem.js
+++ backend/week8/24_react_06_ref_lifecycle/src/PostItem.js
@@ -1,7 +1,7 @@
 
 const PostItem = (props) => {
   // TODO: 부모 컴포넌트에서 넘겨주는 데이터(props) 구조 분해 할당
-  const {id, title, body} = props;
+  const {id, title, body} = props.post;
   return (
     // TODO: 데이터 채우기
     <div className="PostItem">
@@ -9,7 +9,7 @@ const PostItem = (props) => {
         <span className="id">No.{id} - </span>
         <span className="title">{title}</span>
       </div>
-      <p className="body">{body}</p>
+      <p className="body">{body.slice(0,100)} ... </p>
     </div>
   );
 };
--- backend/week8/24_react_06_ref_lifecycle/src/PostList.js
+++ backend/week8/24_react_06_ref_lifecycle/src/PostList.js
@@ -2,24 +2,14 @@ import PostItem from "./PostItem";
 import { useEffect, useState } from "react";
 import Axios from "axios";
 
-// 임시 데이터 (backend 서버에서 받아왔다고 가정하는 데이터)
-let fakePosts = [];
-
 const PostList = () => {
   // TODO: 임시 데이터 (fakePosts)를 저장할 배열 posts state
   // 초기 값은 빈 배열
   const [posts, setPosts] = useState([]);
 
-  const dataURL = "https://jsonplaceholder.typicode.com/posts";
-  const getData = async (url) => {
-    let res;
-    try {
-      res = await Axios.get(url);
-    } catch (e) {
-      console.log(e);
-    }
-    // return res.data;
-    setPosts(res.data.slice(0, 20));
+  const getPosts = async () => {
+    const res = await Axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(res.data.slice(0, 30));
   };
 
   useEffect(() => {
@@ -30,16 +20,19 @@ const PostList = () => {
     //   console.log(fakePosts.length);
     // });
     // setPosts(fakePosts);
-    getData(dataURL);
+    getPosts();
 
     // 2. 함수 실행
 
     console.log("hi");
   }, []);
 
-  // TODO: 해당 컴포넌트가 "mount" 되었을 때
-  // posts state에 fakePosts 데이터를 설정하기
-  // 단, setTimeout()을 이용해 2초 후 posts state에 저장한다.
+  const dataLoading = () => {
+    return <p>loading...</p>;
+  };
+  const dataLoaded = () => {
+    return posts.map((post) => <PostItem key={post.id} post={post} />);
+  };
 
   return (
     <div className="PostList">
@@ -47,13 +40,7 @@ const PostList = () => {
       {/* posts state의 길이에 따라 보여주는 정보 달리하기 (힌트: 삼항 연산자) */}
       {/* posts state 길이가 0 이라면 데이터를 불러오는 중이므로 loading 메세지 */}
       {/* posts state 길이가 0 이 아니라면 데이터를 불러왔으므로 PostItem 컴포넌트 반복 */}
-      {posts.length === 0 ? (
-        <p>loading...</p>
-      ) : (
-        posts.map((data) => (
-          <PostItem id={data.id} title={data.title} body={data.body} />
-        ))
-      )}
+      {posts.length === 0 ? dataLoading() : dataLoaded()}
     </div>
   );
 };
